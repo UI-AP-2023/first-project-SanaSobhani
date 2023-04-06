5e5a32f3dd9d0f0b76b2931d63bfd90b327095ef
SanaSobhani <sanaacc26@gmail.com>
2023-04-07 01:50:24 +0430

    second commit of customer view

--- progect1/src/view/CustomerView.java
+++ progect1/src/view/CustomerView.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class CustomerView {
     private Scanner sc ;
     private CustomerController customerController;
+    private CommodityView commodityView = new CommodityView();
     public CustomerView(){
         sc = new Scanner(System.in);
         customerController = new CustomerController();
@@ -16,12 +17,17 @@ public class CustomerView {
    public void signUpPage(){
        System.out.println("**SignUp Page**");
        System.out.println("Please Enter Your userName, passWord, phoneNumber, eMail");
-       sc.nextLine();
-       for (int i = 0;;i++)
+       boolean signUp = false;
+        while (signUp==false)
        {
-           System.out.println("your password must have at least 8 character includes at least a number");
-           if(customerController.signUp(sc.nextLine(),sc.nextLine(), sc.next(), sc.nextLine())==true)
-               break;
+           System.out.println("your password must have at least 8 character includes at least a number");  //nexline??
+           if(customerController.signUp(sc.nextLine(),sc.nextLine(), sc.nextLine(), sc.nextLine())==false)
+               System.out.println("sign up was not successful");
+           else
+           {
+               signUp=true;
+               System.out.println("a request is sent to admin");
+           }
        }
    }
    public void longInPageAsCustomer()
@@ -30,56 +36,94 @@ public class CustomerView {
        System.out.println("Please Enter Your username, password ");
        sc.nextLine();
        for (int i =0;;i++){
-           if(customerController.login(sc.nextLine(),sc.nextLine())==true)
+           if(customerController.login(sc.nextLine(),sc.nextLine())==true) {
+               userMenu();
+              break;}
+           else
+           {System.out.println("you are not signed up!1. try again 0.go to main page");
+            int choice = sc.nextInt();
+            if(choice==0)
                 break;
+            if(choice==1)
+                sc.nextLine();
+           }
+
        }
    }
    public void userMenu(){
-       System.out.println("1. Visit Account\n2. Show Commodity\n3. Visit Shopping Basket \n4. Increasing Credit\n6. Buy\n5. Shopping History\n6. Exit");
+       System.out.println("**user menu**");
+       int option = 8;
+       while (option!=7){
+           System.out.println("1. Visit Account\n2. Show Commodity\n3. Visit Shopping Basket \n4. Increasing Credit\n5. Buy\n6. Shopping History\n7. Exit");
+           option = sc.nextInt();
+           switch (option){
+               case 1:
+                   visitAccount();
+                   break;
+               case 2:
+                  commodityView.commodityMainView();
+                  break;
+               case 3:
+                   showShoppingBasket();
+                   break;
+               case 4:
+                   bankPanel();
+                   break;
+               case 5:
+                   buyingPanel();
+                   break;
+               case 6:
+                   showCustomerHistory();
+                   break;
+           }
+           System.out.println("**user menu**");
+       }
    }
-   public boolean visitAccount()
+   public void visitAccount()
    {
        System.out.println("**your account**");
        System.out.println(customerController.getCustomerPointer().toString());
        System.out.println("1. Email\n2. Password\n3. PhoneNumber\n4. Exit\n  Enter Feature Number Which You Want to Edit ");
-       int option = sc.nextInt();
-       sc.nextLine();
+       int option = 5;
+       while (option!=4){
+           option = sc.nextInt();
+           sc.nextLine();
        switch (option){
            case(1):
                System.out.println("Enter new Email");
-               String Email = sc.nextLine();
-               if(customerController.editEmail(Email)==false)
-               {
-                    System.out.println("Wrong Email! ");
-                    return false;
+               String email = sc.nextLine();
+               while (customerController.editEmail(email)==false){
+                System.out.println("Wrong Email! enter again");
+                email=sc.nextLine();
                }
+               System.out.println("changes are saved");
                 break;
            case(2):
                System.out.println("Enter new password");
                String passWord = sc.nextLine();
-               if(customerController.editPassWord(passWord)==false)
-               {
-                    System.out.println("Wrong Password! your password must have at least 8 character includes at least a number");
-                    return false;
+               while (customerController.editPassWord(passWord)==false){
+               System.out.println("Wrong Password! your password must have at least 8 character includes at least a number enter again");
+               passWord=sc.nextLine();
                }
+               System.out.println("changes are saved");
                break;
            case(3):
                System.out.println("Enter new phoneNumber");
                String phoneNumber = sc.nextLine();
-               if(customerController.editPhoneNumber(phoneNumber)==false);
-               {
-                   System.out.println("Wrong phone number!");
-                   return false;
+               while (customerController.editPhoneNumber(phoneNumber)==false){
+               System.out.println("Wrong phone number! enter again");
+               phoneNumber=sc.nextLine();
                }
-           case(4):
-               return true;
+               System.out.println("changes are saved");
        }
-      return false;
+       }
    }
    public void bankPanel(){
        System.out.println("**bank panel**");
        System.out.println("please enter amount of credit increase and cvv2 and card password and card number");
-      if(customerController.increasingCredit(sc.nextInt(),sc.nextLine(),sc.nextLine(),sc.nextLine())==true)
+       int credit = sc.nextInt();
+       sc.nextLine();
+      if(customerController.increasingCredit(credit,sc.nextLine(),sc.nextLine(),sc.nextLine())==true)
           System.out.println("SUCCESSFUL\nyour request sent to admin");
       else
           System.out.println("increasing credit was not successful!");
