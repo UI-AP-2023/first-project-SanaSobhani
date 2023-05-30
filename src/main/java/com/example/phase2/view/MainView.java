ef4eafd848b750f5ae0302e4d0c8e7cb1cc398c8
SanaSobhani <sanaacc26@gmail.com>
2023-05-31 00:47:50 +0430

    edit main view

---
+++ src/main/java/com/example/phase2/view/MainView.java
@@ -0,0 +1,43 @@
+package com.example.phase2.view;
+
+import com.example.phase2.model.exception.*;
+
+import java.util.Scanner;
+
+public class MainView {
+    Scanner sc = new Scanner(System.in);
+    CommodityView commodityView = new CommodityView();
+    CustomerView customerView = new CustomerView();
+    AdminView adminView = new AdminView();
+   private void mainMenu()
+   {
+       System.out.println("**MAIN PAGE**");
+       System.out.println("[1] SignUp");
+       System.out.println("[2] Log in as customer");
+       System.out.println("[3] log in as admin");
+       System.out.println("[4] Product Page");
+       System.out.println("[5] Exit");
+       System.out.println("Choose your option");
+   }
+   public void mainView() throws WrongDiscount, UnsuccessfulBuying, InvalidPassword, InvalidEmail, InvalidPhoneNumber {
+       int option = 6;
+       while (option != 5) {
+           mainMenu();
+           option=sc.nextInt();
+           switch (option){
+               case 1:
+                 customerView.signUpPage();
+                 break;
+               case 2:
+                   customerView.longInPageAsCustomer();
+                   break;
+               case 3:
+                 adminView.mainView();
+                 break;
+               case 4:
+                 commodityView.filterView();
+           }
+
+       }
+   }
+}
